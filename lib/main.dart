4e48d9846b43890954ded55609aa43b07939591c
Rosy Waruku <rosy.waruku@strathmore.edu>
2021-10-14 16:30:31 +0300

    recipe details

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'models/recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(RecipeApp());
@@ -24,16 +26,6 @@ class RecipeApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,17 +39,75 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       // 2
       appBar: AppBar(
+        backgroundColor: Colors.grey,
         title: Text(widget.title),
       ),
       // 3
       body: SafeArea(
-        // TODO: Replace child: Container()
         // 4
-        child: Container(),
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11- the child of a gestureDetector
+              // defines the area where the gesture is active
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+
+          },
+        ),
+
       ),
     );
   }
 
-// TODO: Add buildRecipeCard() here
+  Widget buildRecipeCard(Recipe recipe) {
+    // 1
+    return Card(
+      elevation: 2.0,
+      // 2
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          // 3
+          children: <Widget>[
+            // 4
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            // 5
+            Text(
+                recipe.label,
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 20.0,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
 
